5b0b04434edd5c0ff24a9154fec9673550c1272a
e <k13772445714@163.com>
2022-02-17 17:18:54 +0800

    info

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,10 +2,9 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
-    description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+    title: '柯大晨',
+    description: '柯大晨的个人主页.',
+    siteUrl: 'https://kedachen.com', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '@bchiang7',
   },
@@ -20,8 +19,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'BrittanyChiang',
-        short_name: 'BrittanyChiang',
+        name: '柯大晨',
+        short_name: '柯大晨',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
